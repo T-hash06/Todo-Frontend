225e9b6a55d2b1ca83840287e50f74f7478c0b83
Tomas <tomasfelipe.p@gmail.com>
2023-07-09 12:16:04 -0500

    [CREATE] Initial Commit

---
+++ src/components/+Button.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	export let type: 'primary' | 'secondary' = 'primary';
+	export let href: string = '';
+</script>
+
+<button class="button-container {type}">
+	<svelte:element this={href !== '' ? 'a' : 'span'} class="text" {href}><slot /></svelte:element>
+</button>
+
+<style lang="scss">
+	.button-container {
+		position: relative;
+
+		border-radius: 1rem;
+		border: none;
+
+		height: var(--std-height);
+		width: 100%;
+
+		transition-duration: var(--std-transition-time);
+		cursor: pointer;
+
+		.text {
+			transition-duration: inherit;
+
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+			height: 100%;
+			width: 100%;
+
+			border-radius: inherit;
+			text-decoration: none;
+			text-transform: capitalize;
+
+			font-size: 2rem;
+		}
+
+		&.primary {
+			background-color: var(--primary-color);
+			color: var(--secondary-color);
+
+			&:hover,
+			&:active {
+				filter: brightness(1.1);
+			}
+		}
+
+		&.secondary {
+			background-color: transparent;
+			color: var(--primary-color);
+
+			border: 2px solid var(--primary-color);
+
+			.text {
+				color: var(--primary-color);
+			}
+
+			&:hover,
+			&:active {
+				background-color: var(--primary-color);
+
+				.text {
+					color: var(--secondary-color);
+				}
+			}
+		}
+	}
+</style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Button from '$components/+Button.svelte';
+</script>
+
+<div id="login-page" class="page-container">
+	<Button href="home">Hola</Button>
+	<Button href="home" type="secondary">Adios</Button>
+</div>
