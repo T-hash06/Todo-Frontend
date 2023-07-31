4178a5e49d09b8de92ca2ee924b4d778925121a8
Tomas <tomasfelipe.p@gmail.com>
2023-07-31 12:17:34 -0500

    [HOTFIX] Css, project name and version

--- src/routes/api/[...path]/+server.ts
+++ src/routes/api/[...path]/+server.ts
@@ -1,11 +1,11 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-import { BACKEND_ENDPOINT } from '$env/static/private';
+import { BACKEND_ENDPOINT, API_KEY } from '$env/static/private';
 
 function mutateRequest({ params, url, request }: RequestEvent) {
 	const proxyRequest = new Request(`${BACKEND_ENDPOINT}/${params.path + url.search}`, request);
-	proxyRequest.headers.set('x-api-key', 'test');
+	proxyRequest.headers.set('x-api-key', API_KEY);
 
 	return proxyRequest;
 }
